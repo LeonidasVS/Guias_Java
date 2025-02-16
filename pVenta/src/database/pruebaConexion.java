536f518f347703efbfd088d2e60e7e2aefb0b8ae
LeonidasVS <villafrancosibrian@gmail.com>
2025-02-16 16:17:57 -0600

    Guia 5 | Conexion con la base de datos

---
+++ pVenta/src/database/pruebaConexion.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+
+/**
+ *
+ * @author ENVY
+ */
+public class pruebaConexion {
+    public static void main(){
+        Conexion con =new Conexion();
+        con.conectar();
+        if(con.connection!=null){
+            System.out.println("Conectado");
+        }else{
+            System.out.println("Sin conexion");
+        }
+    }
+}
